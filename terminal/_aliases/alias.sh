9eb361e225fb6c2ee2ad0f11edafa2312521c714
David Sancho <dsnxmoreno@gmail.com>
2021-05-06 15:31:04 +0200

    Organize aliases

--- terminal/_aliases/alias.sh
+++ terminal/_aliases/alias.sh
@@ -6,10 +6,8 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# One liners
 alias .="open ."
-alias quit="exit"
-alias export=" export"
-
 alias h="history 1"
 alias o="oni2"
 alias c="code"
@@ -18,12 +16,16 @@ alias y="yarn"
 alias e="esy"
 alias q="query-json"
 alias r=" source \${DOTFILES_PATH}/bin/scripts"
-alias uuid=" source \${DOTFILES_PATH}/bin/utils/uuid_code"
-alias port="source \${DOTFILES_PATH}/bin/system/port_owner"
-
-alias cfg="code \${HOME}/dev/setup"
 alias cl="clear"
+
+# Edit setup
+alias cfg="\${EDITOR} \${DOTFILES_PATH}"
+
+# Opem etc/hosts file
 alias hosts="\${EDITOR} /etc/hosts"
+
+alias quit="exit"
+alias export=" export"
 
 # Detect which `ls` flavor is in use colorflag="-G"
 if ls --color > /dev/null 2>&1; then # GNU `ls`
@@ -80,17 +82,12 @@ command -v md5sum > /dev/null || alias md5sum="md5"
 command -v sha1sum > /dev/null || alias sha1sum="shasum"
 
 # Recursively delete `.DS_Store` files
-alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+alias cleanup="_find . -type f -name '*.DS_Store' -ls -delete"
 
 # Empty the Trash on all mounted volumes and the main HDD.
 # Also, clear Apple’s System Logs to improve shell startup speed.
 # Finally, clear download history from quarantine. https://mths.be/bum
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
-
-# Disable Spotlight
-alias spotoff="sudo mdutil -a -i off"
-# Enable Spotlight
-alias spoton="sudo mdutil -a -i on"
 
 # Airport CLI alias
 alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
@@ -128,23 +125,33 @@ alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
 
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
+# Test zsh startup time
 alias testzsh="TIMEFMT=$'real %E\tuser %U\tsys %S'; repeat 10 {time zsh -i -c exit}"
-
-alias pg='pgcli'
-alias my='mycli'
 
 # Improved clis
 alias copy="pbcopy"
 alias paste="pbpaste"
+alias pg='pgcli'
+alias my='mycli'
 
+# https://github.com/sharkdp/fd
 alias _find="find"
 alias find="fd"
 
+# https://github.com/sindresorhus/fkill-cli
 alias _kill="kill"
 alias kill="fkill"
 
+# https://github.com/sharkdp/bat
 alias _cat="cat"
 alias cat="bat"
 
+# https://github.com/htop-dev/htop
 alias _top="top"
 alias top="htop"
+
+# Generate a uuid
+alias uuid=" uuidgen | tr '[:upper:]' '[:lower:]'"
+
+# Know what process uses the port
+alias port="source \${DOTFILES_PATH}/bin/system/port_owner"
