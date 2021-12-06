b81b3e2a8fa9dc316964a250c104799df734237c
David Sancho Moreno <dsnxmoreno@gmail.com>
2021-12-06 16:16:40 +0100

    chore: Configure exa

--- terminal/_aliases/alias.sh
+++ terminal/_aliases/alias.sh
@@ -14,7 +14,9 @@ alias c="code"
 alias n="npm"
 alias y="yarn"
 alias e="esy"
+alias m="make"
 alias q="query-json"
+alias t="itomate"
 alias r=" source \${DOTFILES_PATH}/bin/scripts"
 alias cl="clear"
 
@@ -27,23 +29,15 @@ alias hosts="code /etc/hosts"
 alias quit="exit"
 alias export=" export"
 
-# Detect which `ls` flavor is in use colorflag="-G"
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-	colorflag="--color"
-	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-else # macOS `ls`
-	colorflag="-G"
-	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
-fi
+# Extend ls
+# alias ls="ls -lhAFrt \${colorflag}"
+alias l="exa --group-directories-first -al --no-time"
+alias ls="exa --group-directories-first -alh --octal-permissions"
 
 # List all files colorized in short format
-alias l="ls -F \${colorflag}"
 
 # List only directories
-alias ld="ls -lF \${colorflag} | grep --color=never '^d'"
-
-# Extend ls
-alias ls="ls -lhAFrt \${colorflag}"
+alias ld="ls -D"
 
 # Always enable colored `grep` output
 # Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
