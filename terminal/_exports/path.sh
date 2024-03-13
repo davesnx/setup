3fe81933fe9bae98cd90d0bc575462ca23a2d709
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-03-13 23:07:20 +0100

    Update vscode stuff

--- terminal/_exports/path.sh
+++ terminal/_exports/path.sh
@@ -2,17 +2,13 @@
 
 paths=(
   "$HOME/bin"
-  "$HOME.deno/bin"
+  "$HOME/.deno/bin"
   "$HOME/.cargo/bin"
   "$HOME/.fly/bin"
   "$DOTFILES_PATH/bin"
   "$DOTFILES_PATH/bin/git-extras"
   "$DOTFILES_PATH/bin/ocaml"
   "$DOTFILES_PATH/bin/system"
-  "$PHP_PATH/bin"
-  "$PHP_PATH/sbin"
-  "$RUBY_PATH/bin"
-  "$GOPATH/bin"
   "$GEM_HOME/bin"
   "$PYTHON_PATH/libexec/bin"
   "/usr/local/opt/openssl@3/bin"
